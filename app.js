9bdb6b1a9b5f73542baa81475c5e1bcc9febc900
valladao <valladao@gmail.com>
2021-06-18 11:41:24 -0300

    Integrate the schema into the Express app

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express")
 const graphqlHTTP = require("express-graphql").graphqlHTTP
+const schema = require("/schema.js")
 
 const app = express()
 
-app.use("/graphql", graphqlHTTP())
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+  })
+)
 
 app.listen(3000, () => {
   console.log("App listening on port 3000")
